7a54b3f0e9cbadba2733967be2897a15cc48364a
Murf-y <charbelfayad64@gmail.com>
2024-03-28 10:35:44 +0200

    ci: coolify

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,11 @@ const nextConfig = {
       },
     ],
   },
+  output: 'standalone',
+  webpack: (config, { isServer }) => {
+    config.resolve.alias['@'] = path.join(__dirname, 'src')
+    return config
+  },
 }
 
 export default nextConfig
